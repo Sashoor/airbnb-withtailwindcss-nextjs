aa2a0697e29793b6777fe3eeb0dd5f1a252aece7
sashoor <saeedashoor.dev@gmail.com>
2022-05-16 22:06:40 +0100

    working on the date picker

--- components/Header.js
+++ components/Header.js
@@ -6,10 +6,28 @@ import {
     UserCircleIcon,
     UserIcon
 } from '@heroicons/react/solid'
+import { useState } from "react"
+
+
+
+
+
 
 function Header() {
+const [searchInput, setSearchInput] = useState("");
+const [startDate, setStartDate] = useState(new Date())
+const [endDate, setEndDate] = useState(new Date())
+const selectionRange = {
+    startDate: startDate,
+    endDate: endDate,
+    key: 'selection'
+}
+
+
+
     return (
         <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
+           {/* <link rel="stylesheet" href="https://unpkg.com/react-day-picker/lib/style.css"> */}
             {/* left-side */}
 
             <div className="relative flex items-center h-10 cursor-pointer my-auto">
@@ -20,7 +38,10 @@ function Header() {
             </div>
             {/* Middle-side  search*/}
             <div className="flex md:border-2 rounded-full py-2 md:shadow-sm ">
-                <input className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-600" type="text" placeholder="Start your search" />
+                <input
+                value={searchInput}
+                onChange={(e) => setSearchInput(e.target.value) }
+                className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-600" type="text" placeholder="Start your search" />
                 <SearchIcon className="hidden md:inline h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2" />
             </div>
 
@@ -35,6 +56,14 @@ function Header() {
                 </div>
             </div>
 
+            {searchInput && (
+                <div>
+                    <DateRangePicker 
+                    ranges={[selectionRange]}
+
+                    />
+                </div>
+            ) }
         </header>
     )
 }
