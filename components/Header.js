63ff05eb4cb52b0d8e6915ee55ec0c5c0478dbdf
sashoor <saeedashoor.dev@gmail.com>
2022-05-17 20:22:30 +0100

    completed the date picker & now working on the Search SSR

--- components/Header.js
+++ components/Header.js
@@ -16,25 +16,32 @@ import { DateRangePicker } from 'react-date-range';
 
 
 function Header() {
-const [searchInput, setSearchInput] = useState("");
-const [startDate, setStartDate] = useState(new Date())
-const [endDate, setEndDate] = useState(new Date())
+    const [searchInput, setSearchInput] = useState("");
+    const [startDate, setStartDate] = useState(new Date())
+    const [endDate, setEndDate] = useState(new Date())
+    const [noOfGuests, setNoOfGuests] = useState(1);
+ 
 
-const handleSelect = (ranges) => {
-setStartDate(ranges.selection.startDate)
-setEndDate(ranges.selection.endDate)
-}
+   const resetInput = () => {
+       setSearchInput("")
+   }
 
-const selectionRange = {
-    startDate: startDate,
-    endDate: endDate,
-    key: 'selection'
-}
+
+    const handleSelect = (ranges) => {
+        setStartDate(ranges.selection.startDate)
+        setEndDate(ranges.selection.endDate)
+    }
+
+    const selectionRange = {
+        startDate: startDate,
+        endDate: endDate,
+        key: 'selection'
+    }
 
 
     return (
         <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
-           {/* <link rel="stylesheet" href="https://unpkg.com/react-day-picker/lib/style.css"> */}
+            {/* <link rel="stylesheet" href="https://unpkg.com/react-day-picker/lib/style.css"> */}
             {/* left-side */}
 
             <div className="relative flex items-center h-10 cursor-pointer my-auto">
@@ -46,9 +53,9 @@ const selectionRange = {
             {/* Middle-side  search*/}
             <div className="flex md:border-2 rounded-full py-2 md:shadow-sm ">
                 <input
-                value={searchInput}
-                onChange={(e) => setSearchInput(e.target.value) }
-                className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-600" type="text" placeholder="Start your search" />
+                    value={searchInput}
+                    onChange={(e) => setSearchInput(e.target.value)}
+                    className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-600" type="text" placeholder="Start your search" />
                 <SearchIcon className="hidden md:inline h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2" />
             </div>
 
@@ -63,22 +70,36 @@ const selectionRange = {
                 </div>
             </div>
 
+{/* Search input and the calander */}
+
             {searchInput && (
                 <div className="flex flex-col col-span-3 mx-auto">
-                    <DateRangePicker 
-                    ranges={[selectionRange]}
-                    minDate={new Date()}
-                    rangeColors={["#FD5B61"]}
-                    onChange={handleSelect}
+                    <DateRangePicker
+                        ranges={[selectionRange]}
+                        minDate={new Date()}
+                        rangeColors={["#FD5B61"]}
+                        onChange={handleSelect}
                     />
-                      <div className="flex items-center border-b mb-4">
-                    <h2 className="text-2xl flex-grow font-semibold">Number of Guests</h2>
+                    <div className="flex items-center border-b mb-4">
+                        <h2 className="text-2xl flex-grow font-semibold">Number of Guests</h2>
 
-                    <UserIcon className="h-5" />
-                    <input type ="number" className="w-12 pl-2 text-lg outline-none text-red-400"/>
-                     </div>
+                        <UserIcon className="h-5" />
+                        <input
+                            value={noOfGuests}
+                            onChange={(e) => setNoOfGuests(e.target.value)}
+                            type="number"
+                            min={1}
+                            className="w-12 pl-2 text-lg outline-none text-red-400" />
+                    </div>
+
+
+                    <div className="flex bg-green-200">
+                        <button onClick={resetInput} className="flex-grow text-gray-500">Cancel</button>
+                        <button className="flex-grow text-red-400">Search</button>
+                    </div>
+
                 </div>
-              
+
             )}
         </header>
     )
