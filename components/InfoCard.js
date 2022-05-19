1ed56bc371807f1fc3001fb111d22a50f9cc9c95
sashoor <saeedashoor.dev@gmail.com>
2022-05-19 22:20:32 +0100

    completed the search component and also added progress bar

--- components/InfoCard.js
+++ components/InfoCard.js
@@ -5,9 +5,9 @@ import { StarIcon } from '@heroicons/react/solid'
 
 function InfoCard({ img, location, title, description, star, price, total }) {
     return (
-        <div className="flex py-7 px-2 border-b cursor-pointer hover:opacity-80 hover:shadow-lg transition duration-200 ease-out first:border-t ">
+        <div className="flex py-7 px-2 border-b cursor-pointer hover:opacity-80 hover:shadow-lg transition duration-200 ease-out first:border-t">
             <div className="relative h-24 w-40 md:h-52 md:w-80 flex-shrink-0">
-                <Image
+                <Image className="rounded-2xl"
                     src={img}
                     layout="fill" objectFit="cover"
                 />
